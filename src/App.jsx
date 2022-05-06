43586aa61819f77581ecbba744b38e2ac2594498
Safira Baptista Alves <safbapalv@gmail.com>
2022-05-06 14:12:08 -0300

    removi src/app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import home from './paginas/home'
 import usuario from './paginas/usuario'
 import favoritos from './paginas/favoritos'
@@ -10,18 +10,20 @@ function App() {
 
   return (
     <Router>
-        <Route exact path='/home' element={ home }>
+      <Switch>
+        <Route path='/home' exact element={ home }>
           <home />
         </Route>
-        <Route exact path='/usuario' element={ usuario }>
+        <Route path='/usuario' element={ usuario }>
           <usuario />
         </Route>
-        <Route exact path='/favoritos' element={ favoritos }>
+        <Route path='/favoritos' element={ favoritos }>
           <favoritos />
         </Route>
-        <Route exact path='/pagina404' element={ pagina404 }>
+        <Route path='/pagina404' element={ pagina404 }>
           <pagina404 />
         </Route>
+      </Switch>
     </Router>
   )
 }
